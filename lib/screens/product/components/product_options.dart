c70f298169c3e23119233193e3c7398f25abdb98
Brandon Tan <30314729+Brandon0514@users.noreply.github.com>
2023-03-19 22:14:41 +0800

    feat(): view product ui revamp

--- lib/screens/product/components/product_options.dart
+++ lib/screens/product/components/product_options.dart
@@ -17,98 +17,60 @@ class ProductOption extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 200,
+      height: 350,
       child: Stack(
         children: <Widget>[
           Positioned(
-            left: 16.0,
+            left: 70.0,
+            top: -25,
             child: SvgPicture.asset(
               product.image,
-              height: 200,
-              width: 200,
+              height: 400,
+              width: 400,
             ),
           ),
           Positioned(
-            right: 0.0,
+            left: 0,
+            top: 0,
+            bottom: 0,
             child: Container(
               height: 180,
-              width: 300,
+              width: MediaQuery.of(context).size.width / 2,
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                // mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    padding: const EdgeInsets.only(left: 40, top: 80),
                     child: Text(product.id,
-                        textAlign: TextAlign.right,
+                        textAlign: TextAlign.left,
                         style: TextStyle(
-                            fontSize: 20,
+                            fontSize: 32,
                             fontWeight: FontWeight.bold,
                             color: Colors.white,
                             shadows: shadow)),
                   ),
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    padding: const EdgeInsets.only(left: 40, top: 30),
                     child: Text(product.name,
-                        textAlign: TextAlign.right,
+                        textAlign: TextAlign.left,
                         style: TextStyle(
+                            fontSize: 15,
                             fontWeight: FontWeight.bold,
                             color: Colors.white,
                             shadows: shadow)),
                   ),
-                  InkWell(
-                    onTap: () async {
-                      Navigator.of(context).push(
-                          MaterialPageRoute(builder: (_) => CheckOutPage()));
-                    },
-                    child: Container(
-                      width: MediaQuery.of(context).size.width / 2.5,
-                      decoration: BoxDecoration(
-                          color: Colors.red,
-                          gradient: mainButton,
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10.0),
-                              bottomLeft: Radius.circular(10.0))),
-                      padding: EdgeInsets.symmetric(vertical: 16.0),
-                      child: Center(
-                        child: Text(
-                          'Buy Now',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      scaffoldKey.currentState!.showBottomSheet((context) {
-                        return ShopBottomSheet();
-                      });
-                    },
-                    child: Container(
-                      width: MediaQuery.of(context).size.width / 2.5,
-                      decoration: BoxDecoration(
-                          color: Colors.red,
-                          gradient: mainButton,
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10.0),
-                              bottomLeft: Radius.circular(10.0))),
-                      padding: EdgeInsets.symmetric(vertical: 16.0),
-                      child: Center(
-                        child: Text(
-                          'Add to cart',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                  )
                 ],
               ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            bottom: 0,
+            child: Divider(
+              thickness: 1,
+              color: Colors.white,
+              height: 10,
             ),
           )
         ],
